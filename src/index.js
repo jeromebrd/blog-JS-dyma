cbb354501e5701682ac29187a065dcfc36cc4485
Jerome <jerome.bourscheid@gmail.com>
2022-09-14 15:42:30 +0200

    Added a delete modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './assets/styles/styles.scss';
 import './index.scss';
+import { openModal } from './assets/javascripts/modal.js';
 
 const articleContainerElement = document.querySelector('.articles-container');
 const categoriesContainerElement = document.querySelector('.categories');
@@ -66,21 +67,22 @@ const createArticles = () => {
 
   deleteButtons.forEach((button) => {
     button.addEventListener('click', async (event) => {
-      try {
-        const target = event.target;
-        const articleId = target.dataset.id;
-        const response = await fetch(
-          `https://restapi.fr/api/article/${articleId}`,
-          {
-            method: 'DELETE',
-          }
-        );
-        const body = await response.json();
-        console.log(body);
-        fetchArticle();
-      } catch (e) {
-        console.log('e :', e);
-      }
+      openModal('Etes-vous sûr de vouloir supprimer votre article ?');
+      // try {
+      //   const target = event.target;
+      //   const articleId = target.dataset.id;
+      //   const response = await fetch(
+      //     `https://restapi.fr/api/article/${articleId}`,
+      //     {
+      //       method: 'DELETE',
+      //     }
+      //   );
+      //   const body = await response.json();
+      //   console.log(body);
+      //   fetchArticle();
+      // } catch (e) {
+      //   console.log('e :', e);
+      // }
     });
   });
 };
